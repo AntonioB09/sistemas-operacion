cd11804ba31b23b67ed450909b117533aa3683d9
Tomas <tomasg92@gmail.com>
2016-11-10 20:10:08 -0400

    Compare multi-process execution time vs single-process execution time

--- multiproc.py
+++ multiproc.py
@@ -1,12 +1,25 @@
 from multiprocessing import Process
 import os
+import time
 
-def cpu_eat():
-    n = 0
-    
-    while True:
-        n += 1
+k = 10000000
 
+def loop_print(n):
+    for i in range(0, n):
+        i += 1
+        
+multi_start = time.time()
 for i in range(0, 4):
-    p = Process(target=cpu_eat)
+    p = Process(target=loop_print, args=(k,))
     p.start()
+multi_end = time.time()
+multi_elapsed = multi_end - multi_start
+
+single_start = time.time()
+for i in range(0, 4):
+    loop_print(k)
+single_end = time.time()
+single_elapsed = single_end - single_start
+
+print("Multi time: {} seconds".format(multi_elapsed))
+print("Single time: {} seconds".format(single_elapsed))
